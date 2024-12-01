091f6836dc77ab90a4acb7dfcb1804fd061a42ac
Batuhanbass <batuhan.bas.yzm@gmail.com>
2024-12-01 22:34:14 +0300

    Add project files.

---
+++ Hata_Yönetimi/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Hata_Yönetimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            List<string> students = new List<string> { "Batuhan", "Ahmet", "Mehmet", "Derin" };
+            if (!students.Contains("Ahme2t"))
+            {
+
+                throw new RecordNotFoundExceptiion("record not found!");
+
+
+            }
+            else { 
+            
+            
+            }
+
+            HandleException(() =>
+            {
+                //Find();
+            }); 
+        }
+
+
+        private static void HandleException(Action action) {
+            try { }
+            catch(Exception exception) { }
+            
+        }
+    }
+}
---
+++ Hata_Yönetimi/RecordNotFoundExceptiion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hata_Yönetimi
+{
+    public class RecordNotFoundExceptiion:Exception{
+        
+        public RecordNotFoundExceptiion(string massage):base(massage){
+            Console.WriteLine(massage);  
+        
+        
+        }
+      
+    }
+}
